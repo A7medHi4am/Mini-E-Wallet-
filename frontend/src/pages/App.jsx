13a98a5b7753d21866347c57ec0ce0a49b5ff9c5
gamilatoam2005 <gamilatoam@gmail.com>
2026-07-15 12:58:10 +0300

    Create App.jsx

---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,5 @@
+import { Route } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+
+// Inside your Routes component
+<Route path="/admin" element={<AdminDashboard />} />
